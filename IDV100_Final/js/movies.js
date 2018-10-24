4a8414f4362a7ab51c16bf9259091cdf90b76773
Julez137 <36993671+Julez137@users.noreply.github.com>
2018-10-24 20:15:02 +0200

    Initialize API variables(FAIL)

--- IDV100_Final/js/movies.js
+++ IDV100_Final/js/movies.js
@@ -1,10 +1,8 @@
 $(function() {
     var apiRequest = new XMLHttpRequest();
-    var apiRequestPoster = new XMLHttpRequest();
     
     apiRequest.open('GET', 'http://www.omdbapi.com/?i=tt3896198&apikey=462d8ac2', true);
 
-    
     
     apiRequest.onload = function(){
          data = JSON.parse(this.response);
@@ -12,14 +10,21 @@ $(function() {
         
     }
     
-
     
-   
-    
-    
-    
-    
-    
+    function createContainers(Title, Year, Rated, Released, Runtime, Genre, Director, Actors, Plot, Poster){
+        $(".nav-con-inner").append("<div class='movie'><div class='image'><img src=" + Poster + "></div><div class='text'>" + Title+ "</div></div>");
+        
+        localStorage.setItem("title", Title);
+        localStorage.setItem("year", Year);
+        localStorage.setItem("rated", Rated);
+        localStorage.setItem("released", Released);
+        localStorage.setItem("runtime", Runtime);
+        localStorage.setItem("genre", Genre);
+        localStorage.setItem("director", Director);
+        localStorage.setItem("actors", Actors);
+        localStorage.setItem("plot", Plot);
+        localStorage.setItem("poster", Poster);
+    }
     
     
     
